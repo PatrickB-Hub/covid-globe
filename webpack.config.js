83ca3f79d15fb863ba015f2512922b565d2cc226
JayShiznit <12658037+PatrickB-Hub@users.noreply.github.com>
2020-08-25 21:50:55 +0100

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = {
+  devtool: "eval-source-map",
+  entry: "./src/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      // html
+      {
+        test: /\.html$/,
+        use: {
+          loader: "html-loader",
+          options: { minimize: true }
+        }
+      },
+      // typescript
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        include: [path.resolve(__dirname, "src")]
+      },
+      // assets
+      {
+        test: /\.(png|svg|jpg|gif|ttf|woff|json)$/,
+        use: [
+          "file-loader"
+        ]
+      },
+      // sass
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new BundleAnalyzerPlugin()
+  ]
+}
